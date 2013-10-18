c70793cdbd57c85da977377208d1174c1ef9ba57
niginder <nicolas.2.ginder@nokia.com>
2013-10-18 16:08:14 +0100

    create elb with health check

--- src/shuppet/loadbalancer.clj
+++ src/shuppet/loadbalancer.clj
@@ -7,41 +7,87 @@
    [clojure.zip :as zip]
    [clojure.data.zip.xml :refer [xml1-> attr xml-> text text= attr=]]))
 
+(def subnet1 "subnet-24df904c")
+(def subnet2 "subnet-bdc08fd5")
+
+(def elb-config-modified
+  { "LoadBalancerName" "nico-test-elb"
+    "Listerners" [{"LoadBalancerPort" 8080
+                   "InstancePort" 8080
+                   "Protocol" "http"
+                   "InstanceProtocol" "http"}]
+    "Subnets" [subnet1 subnet2]
+    "Scheme" "internal"
+    "HealthCheck" {"Target" "HTTP:8080/1.x/ping"
+                   "HealthyThreshold" 2
+                   "UnhealthyThreshold" 2
+                   "Interval" 6
+                   "Timeout" 5}})
 
 
-; get 200 when same config
-#(elb-request {"Action" "CreateLoadBalancer"
-              "LoadBalancerName" "recommendations"
-              "Listeners.member.1.LoadBalancerPort" 8080
-              "Listeners.member.1.InstancePort"  8080
-              "Listeners.member.1.Protocol" "http"
-              "Listeners.member.1.InstanceProtocol" "http"
-              "Listeners.member.2.LoadBalancerPort" 80
-              "Listeners.member.2.InstancePort"  8080
-              "Listeners.member.2.Protocol" "http"
-              "Listeners.member.2.InstanceProtocol" "http"
-              "Subnets.member.1" "subnet-24df904c"
-              "Subnets.member.2" "subnet-bdc08fd5"
-              "Scheme" "internal"
-              "SecurityGroups.member.1" "recommendations-lb"
-              "AvailabilityZones.member.1" "eu-west-1a"
-              "AvailabilityZones.member.2" "eu-west-1b"})
+(defn to-aws-format [config]
+
+  )
 
 
-#(elb-request {"Action" "CreateLoadBalancerListeners"
-               "LoadBalancerName" "recommendations"
-               "Listeners.member.1.LoadBalancerPort" 80
-               "Listeners.member.1.InstancePort" 8080
-               "Listeners.member.1.InstanceProtocol" "http"
-               "Listeners.member.1.Protocol" "http"})
+(def elb-config { "LoadBalancerName" "nico-test-elb"
+                  "Listeners.member.1.LoadBalancerPort" 8080
+                  "Listeners.member.1.InstancePort"  8080
+                  "Listeners.member.1.Protocol" "http"
+                  "Listeners.member.1.InstanceProtocol" "http"
+                  "Subnets.member.1" subnet1
+                  "Subnets.member.2" subnet2
+                  "Scheme" "internal"
+                  ;health
+                  "HealthCheck.Target" "HTTP:8080/1.x/ping"
+                  "HealthCheck.HealthyThreshold" 2
+                  "HealthCheck.UnhealthyThreshold" 2
+                  "HealthCheck.Interval" 6
+                  "HealthCheck.Timeout" 5
+                  "SecurityGroups.member.1" "nicoelbtest"
+                                        ; "AvailabilityZones.member.1" "eu-west-1a"
+                                        ;"AvailabilityZones.member.2" "eu-west-1b"
 
-#(elb-request {"Action" "DeleteLoadBalancerListeners"
-              "LoadBalancerName" "recommendations"
-              "LoadBalancerPorts.member.1" "80"})
+                  })
+
+(defn create-elb [config]
+  ;create security group first for elb
+  (elb-request (merge {"Action" "CreateLoadBalancer"} (select-keys config
+                                                             ["LoadBalancerName"
+                                                              "Listeners.member.1.LoadBalancerPort"
+                                                              "Listeners.member.1.InstancePort"
+                                                              "Listeners.member.1.Protocol"
+                                                              "Listeners.member.1.InstanceProtocol"
+                                                              "Subnets.member.1"
+                                                              "Subnets.member.2"
+                                                              "SecurityGroups.member.1"
+                                                              "Scheme" "internal"])))
+  (elb-request (merge {"Action" "ConfigureHealthCheck"} (select-keys config
+                                                                     ["LoadBalancerName"
+                                                                      "HealthCheck.Target"
+                                                                      "HealthCheck.HealthyThreshold"
+                                                                      "HealthCheck.UnhealthyThreshold"
+                                                                      "HealthCheck.Interval"
+                                                                      "HealthCheck.Timeout"]))))
 
 
-#(elb-request {"Action" "DescribeLoadBalancers"
-               "LoadBalancerNames.member.1" "recommendations"})
+(defn find-elb [name]
+  (elb-request {"Action" "DescribeLoadBalancers"
+                "LoadBalancerNames.member.1" name}))
 
-#(elb-request {"Action" "DeleteLoadBalancer"
-              "LoadBalancerName" "test-elb-nico"})
+(defn- create-listeners []
+  (elb-request {"Action" "CreateLoadBalancerListeners"
+                "LoadBalancerName" "recommendations"
+                "Listeners.member.1.LoadBalancerPort" 80
+                "Listeners.member.1.InstancePort" 8080
+                "Listeners.member.1.InstanceProtocol" "http"
+                "Listeners.member.1.Protocol" "http"}))
+
+(defn- delete-listerners []
+  (elb-request {"Action" "DeleteLoadBalancerListeners"
+                "LoadBalancerName" "recommendations"
+                "LoadBalancerPorts.member.1" "80"}))
+
+(defn- delete-elb []
+  (elb-request {"Action" "DeleteLoadBalancer"
+                "LoadBalancerName" "test-elb-nico"}))
