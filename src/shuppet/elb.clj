d081c7a7e96a88862a925ea2e059b036ec3e682b
niginder <nicolas.2.ginder@nokia.com>
2013-10-21 14:55:20 +0100

    conversion to aws format

--- src/shuppet/elb.clj
+++ src/shuppet/elb.clj
@@ -1,4 +1,4 @@
-(ns shuppet.loadbalancer
+(ns shuppet.elb
   (:require
    [shuppet.aws :refer [elb-request]]
    [clj-http.client :as client]
@@ -7,57 +7,37 @@
    [clojure.zip :as zip]
    [clojure.data.zip.xml :refer [xml1-> attr xml-> text text= attr=]]))
 
-(def subnet1 "subnet-24df904c")
-(def subnet2 "subnet-bdc08fd5")
+(defn map-to-dot [prefix m]
+  (map (fn [[k v]] [(str prefix "." k) v])
+       m))
 
-(defn map-to-dot [prefix map]
-  )
+(defn to-member [prefix i]
+  (str prefix ".member." i))
 
 (defn list-to-member [prefix list]
-  (apply hash-map (flatten (map #(do [(str prefix ".member." %1) %2]) (iterate inc 1) list))))
-
-(def elb-config-modified
-  { "LoadBalancerName" "nico-test-elb"
-    "Listerners" [{"LoadBalancerPort" 8080
-                   "InstancePort" 8080
-                   "Protocol" "http"
-                   "InstanceProtocol" "http"}]
-    "Subnets" [subnet1 subnet2]
-    "Scheme" "internal"
-    "HealthCheck" {"Target" "HTTP:8080/1.x/ping"
-                   "HealthyThreshold" 2
-                   "UnhealthyThreshold" 2
-                   "Interval" 6
-                   "Timeout" 5}})
-
+  (flatten (map (fn [i v]
+                  (cond
+                   (map? v) (map-to-dot (to-member prefix i) v)
+                   :else [(to-member prefix i) v]))
+                (iterate inc 1)
+                list)))
 
 (defn to-aws-format [config]
-
-  )
-
-
-(def elb-config { "LoadBalancerName" "nico-test-elb"
-                  "Listeners.member.1.LoadBalancerPort" 8080
-                  "Listeners.member.1.InstancePort"  8080
-                  "Listeners.member.1.Protocol" "http"
-                  "Listeners.member.1.InstanceProtocol" "http"
-                  "Subnets.member.1" subnet1
-                  "Subnets.member.2" subnet2
-                  "Scheme" "internal"
-                  ;health
-                  "HealthCheck.Target" "HTTP:8080/1.x/ping"
-                  "HealthCheck.HealthyThreshold" 2
-                  "HealthCheck.UnhealthyThreshold" 2
-                  "HealthCheck.Interval" 6
-                  "HealthCheck.Timeout" 5
-                  "SecurityGroups.member.1" "nicoelbtest"
-                                        ; "AvailabilityZones.member.1" "eu-west-1a"
-                                        ;"AvailabilityZones.member.2" "eu-west-1b"
-
-                  })
+  (apply hash-map (flatten (map (fn [[k v]]
+                                  (cond (sequential? v) (list-to-member k v)
+                                        (map? v) (map-to-dot k v)
+                                        :else [k v])
+                                  )
+                                config))))
 
 (defn create-elb [config]
-  ;create security group first for elb
+                                        ;create security group first for elb
+
+
+  (let [elb (dissoc config "HealthCheck")
+        health-check (select-keys config ["LoadBalancerName" "HealthCheck"])])
+
+
   (elb-request (merge {"Action" "CreateLoadBalancer"} (select-keys config
                                                              ["LoadBalancerName"
                                                               "Listeners.member.1.LoadBalancerPort"
---
+++ test/shuppet/unit/elb.clj
@@ -0,0 +1,40 @@
+(ns shuppet.unit.elb
+  (:require [cheshire.core :as json])
+  (:use [shuppet.elb]
+        [midje.sweet]))
+
+(def subnet1 "subnet-24df904c")
+(def subnet2 "subnet-bdc08fd5")
+
+(fact-group :unit
+
+            (fact "correctly convert to aws format"
+                  (let [config  {"LoadBalancerName" "testelb"
+                                 "Listeners" [{"LoadBalancerPort" 8080
+                                                "InstancePort" 8080
+                                                "Protocol" "http"
+                                                "InstanceProtocol" "http"}]
+                                 "Subnets" [subnet1 subnet2]
+                                 "SecurityGroups" ["default"]
+                                 "Scheme" "internal"
+                                 "HealthCheck" {"Target" "HTTP:8080/1.x/ping"
+                                                "HealthyThreshold" 2
+                                                "UnhealthyThreshold" 2
+                                                "Interval" 6
+                                                "Timeout" 5}}
+                        converted-config { "LoadBalancerName" "testelb"
+                                           "Listeners.member.1.LoadBalancerPort" 8080
+                                           "Listeners.member.1.InstancePort"  8080
+                                           "Listeners.member.1.Protocol" "http"
+                                           "Listeners.member.1.InstanceProtocol" "http"
+                                           "Subnets.member.1" subnet1
+                                           "Subnets.member.2" subnet2
+                                           "Scheme" "internal"
+                                           "HealthCheck.Target" "HTTP:8080/1.x/ping"
+                                           "HealthCheck.HealthyThreshold" 2
+                                           "HealthCheck.UnhealthyThreshold" 2
+                                           "HealthCheck.Interval" 6
+                                           "HealthCheck.Timeout" 5
+                                           "SecurityGroups.member.1" "default"}]
+
+                    (to-aws-format config) => converted-config)))
