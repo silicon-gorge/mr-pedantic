9b81998cba1f326d736bb7b1cd3bc00790cd3e6b
niginder <nicolas.2.ginder@nokia.com>
2013-10-29 15:11:26 +0000

    use var for elb sg

--- shared/test.clj
+++ shared/test.clj
@@ -5,12 +5,14 @@
   [(sg-rule "tcp" 80 8090 ["198.0.2.0/24" "198.51.100.0/24"])
    (sg-rule "udp" 80 8090 ["198.0.2.0/24" "198.51.100.0/32"])])
 
+(def elb-sg (str app-name "-lb"))
+
 {:sg [{:GroupName app-name
        :GroupDescription (str "Security group for application " app-name)
        :VpcId vpc-id
        :Ingress sg-ingress
        :Egress sg-egress}
-      {:GroupName (str app-name "-lb")
+      {:GroupName elb-sg
        :GroupDescription (str "Security group for load balancer " app-name)
        :VpcId vpc-id
        :Ingress override-sg-ingress
@@ -19,7 +21,7 @@
  :elb {:LoadBalancerName app-name
        :Listeners [(elb-listener 8080 8080 "http")
                    (elb-listener 80 8080 "http")]
-       :SecurityGroups [(str app-name "-sg")]
+       :SecurityGroups [elb-sg]
        :Subnets elb-subnets
        :Scheme "internal"
        :HealthCheck (elb-healthcheck "HTTP:8080/1.x/ping" 2 2 6 5)}
