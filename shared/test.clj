5ccff91c0ca9f98a1b9f93e9ba7afdc315c97337
niginder <nicolas.2.ginder@nokia.com>
2013-10-29 14:17:34 +0000

    use new config functions for elb

--- shared/test.clj
+++ shared/test.clj
@@ -1,36 +1,27 @@
 (def override-sg-ingress
-  [(group-record "tcp" 80 8080 '("198.0.2.0/24" "198.51.100.0/24"))])
+  [(sg-rule "tcp" 80 8080 '("198.0.2.0/24" "198.51.100.0/24"))])
 
 (def override-sg-egress
-  [(group-record "tcp" 80 8090 '("198.0.2.0/24" "198.51.100.0/24"))
-   (group-record "udp" 80 8090 '("198.0.2.0/24" "198.51.100.0/32"))])
-
+  [(sg-rule "tcp" 80 8090 ["198.0.2.0/24" "198.51.100.0/24"])
+   (sg-rule "udp" 80 8090 ["198.0.2.0/24" "198.51.100.0/32"])])
 
 {:sg [{:GroupName app-name
        :GroupDescription (str "Security group for application " app-name)
        :VpcId vpc-id
-       :Ingress override-sg-ingress
-       :Egress override-sg-egress }
+       :Ingress sg-ingress
+       :Egress sg-egress}
       {:GroupName (str app-name "-lb")
        :GroupDescription (str "Security group for load balancer " app-name)
        :VpcId vpc-id
        :Ingress override-sg-ingress
        :Egress override-sg-egress }]
+
  :elb {:LoadBalancerName app-name
-       :Listeners [{:LoadBalancerPort "8080"
-                    :InstancePort "8080"
-                    :Protocol "http"
-                    :InstanceProtocol "http"}
-                   {:LoadBalancerPort "80"
-                    :InstancePort "8080"
-                    :Protocol "http"
-                    :InstanceProtocol "http"}]
-       :SecurityGroups [(str app-name "-lb")]
+       :Listeners [(elb-listener 8080 8080 "http")
+                   (elb-listener 80 8080 "http")]
+       :SecurityGroups [(str app-name "-sg")]
        :Subnets elb-subnets
        :Scheme "internal"
-       :HealthCheck {:Target "HTTP:8080/1.x/ping"
-                     :HealthyThreshold "2"
-                     :UnhealthyThreshold "2"
-                     :Interval "6"
-                     :Timeout "5"}}
+       :HealthCheck (elb-healthcheck "HTTP:8080/1.x/ping" 2 2 6 5)}
+
  :campfire [campfire-room]}
