67dd13e857d8338011861ed0969f16acd9243614
niginder <nicolas.2.ginder@nokia.com>
2013-10-29 17:22:25 +0000

    move app specific vars

--- shared/local.clj
+++ shared/local.clj
@@ -3,5 +3,3 @@
 (def sg-ingress [(sg-rule "tcp" 80 8080 ["10.216.221.0/24" "10.83.1.0/24"])])
 (def sg-egress  [(sg-rule "-1" ["0.0.0.0/0"])])
 (def elb-subnets ["subnet-24df904c" "subnet-bdc08fd5"])
-(def elb-8080->8080 (elb-listener 8080 8080 "http"))
-(def elb-healthcheck-ping (elb-healthcheck "HTTP:8080/1.x/ping" 2 2 6 5))
--- shared/test.clj
+++ shared/test.clj
@@ -1,18 +1,20 @@
+(def elb-8080->8080 (elb-listener 8080 8080 "http"))
+(def elb-healthcheck-ping (elb-healthcheck "HTTP:8080/1.x/ping" 2 2 6 5))
+(def elb-sg-name (str app-name "-lb"))
+
 (def override-sg-ingress
-  [(sg-rule "tcp" 80 8080 '("198.0.2.0/24" "198.51.100.0/24"))])
+  [(sg-rule "tcp" 80 8080 ["198.0.2.0/24" "198.51.100.0/24"])])
 
 (def override-sg-egress
   [(sg-rule "tcp" 80 8090 ["198.0.2.0/24" "198.51.100.0/24"])
    (sg-rule "udp" 80 8090 ["198.0.2.0/24" "198.51.100.0/32"])])
-
-(def elb-sg (str app-name "-lb"))
 
 {:SecurityGroups [{:GroupName app-name
                    :GroupDescription (str "Security group for application " app-name)
                    :VpcId vpc-id
                    :Ingress sg-ingress
                    :Egress sg-egress}
-                  {:GroupName elb-sg
+                  {:GroupName elb-sg-name
                    :GroupDescription (str "Security group for load balancer " app-name)
                    :VpcId vpc-id
                    :Ingress override-sg-ingress
@@ -21,7 +23,7 @@
  :LoadBalancer {:LoadBalancerName app-name
                 :Listeners [elb-8080->8080
                             (elb-listener 80 8080 "http")]
-                :SecurityGroups [elb-sg]
+                :SecurityGroups [elb-sg-name]
                 :Subnets elb-subnets
                 :Scheme "internal"
                 :HealthCheck elb-healthcheck-ping}
