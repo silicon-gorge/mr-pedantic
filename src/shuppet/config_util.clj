798fd7836b98f9f617f2a0252545da7146e19d4a
niginder <nicolas.2.ginder@nokia.com>
2013-10-30 13:42:51 +0000

    Neil can't stand bad formatting

--- src/shuppet/config_util.clj
+++ src/shuppet/config_util.clj
@@ -4,20 +4,19 @@
 (defn elb-listener
   "http://docs.aws.amazon.com/ElasticLoadBalancing/latest/APIReference/API_Listener.html"
   [elb-port instance-port protocol]
-  (without-nils
-   {:LoadBalancerPort elb-port
-    :InstancePort instance-port
-    :Protocol protocol
-    :InstanceProtocol protocol}))
+  (without-nils {:LoadBalancerPort elb-port
+                 :InstancePort instance-port
+                 :Protocol protocol
+                 :InstanceProtocol protocol}))
 
 (defn elb-healthcheck
   "http://docs.aws.amazon.com/ElasticLoadBalancing/latest/APIReference/API_HealthCheck.html"
   [target healthy-threshold unhealthy-threshold interval timeout]
   (without-nils {:Target target
-                      :HealthyThreshold healthy-threshold
-                      :UnhealthyThreshold unhealthy-threshold
-                      :Interval interval
-                      :Timeout timeout}))
+                 :HealthyThreshold healthy-threshold
+                 :UnhealthyThreshold unhealthy-threshold
+                 :Interval interval
+                 :Timeout timeout}))
 
 (defn sg-rule
   "Creates a Ingress/Egress config for a security group"
