0f84eb6a82b206cdd3dc530d8db70b5730039cfe
niginder <nicolas.2.ginder@nokia.com>
2013-11-15 11:50:51 +0000

    test shuppet customization

--- test/shuppet/unit/core_shuppet.clj
+++ test/shuppet/unit/core_shuppet.clj
@@ -2,3 +2,26 @@
   (:require  [slingshot.slingshot :refer [try+ throw+]])
   (:use [shuppet.core-shuppet]
         [midje.sweet]))
+
+(deftype TestAppNames []
+  ApplicationNames
+  (list-names
+    [_]
+    ["test-app"]))
+
+(deftype TestConfig []
+  Configuration
+  (as-string
+    [_ environment name]
+    "this is a test config"))
+
+(def configuration @#'shuppet.core-shuppet/configuration)
+
+(fact-group :unit
+            (fact "app list can be customized"
+                  (binding [*application-names*  (TestAppNames.)]
+                    (app-names) => ["test-app"]))
+
+            (fact "configuration source can be customized"
+                  (binding [*configuration*  (TestConfig.)]
+                    (configuration ..env.. ..name..) => "this is a test config")))
