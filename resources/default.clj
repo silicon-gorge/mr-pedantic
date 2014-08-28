65e5315610cd67995901b07a8e5e81fa1200a7a2
Neil Prosser <neil.prosser@nokia.com>
2014-08-28 11:42:39 +0100

    Thin down default configuration

--- resources/default.clj
+++ resources/default.clj
@@ -1,7 +1,1 @@
-;Here we define a configuration as a Clojure map, it must be at the end of the file to be used by Shuppet.
-{
-;IAM role doc http://docs.aws.amazon.com/IAM/latest/APIReference/Welcome.html
- :Role {:RoleName $app-name
-        ; This tool http://awspolicygen.s3.amazonaws.com/policygen.html helps to create policy documents
-        }
- }
+{:Role {:RoleName $app-name}}
