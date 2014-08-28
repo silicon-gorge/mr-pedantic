b50cbb43eb229557226ba42ccecbb3c6f77f7a7a
Neil Prosser <neil.prosser@nokia.com>
2014-08-28 22:43:58 +0100

    Tests for campfire namespace

--- src/shuppet/campfire.clj
+++ src/shuppet/campfire.clj
@@ -1,12 +1,17 @@
 (ns shuppet.campfire
-  (:require [slingshot.slingshot :refer [try+ throw+]]
-            [environ.core :as env]
-            [clj-campfire.core :as cf]))
+  (:require [clj-campfire.core :as cf]
+            [environ.core :refer [env]]))
 
 (def ^:private cf-settings
-  {:api-token (env/env :service-campfire-api-token)
+  {:api-token (env :service-campfire-api-token)
    :ssl true
-   :sub-domain  (env/env :service-campfire-sub-domain)})
+   :sub-domain  (env :service-campfire-sub-domain)})
+
+(def ^:private campfire-off?
+  (Boolean/valueOf (env :service-campfire-off)))
+
+(def ^:private default-info-room
+  (env :service-campfire-default-info-room))
 
 (defn room
   "Sets up the room for sending messages"
@@ -14,25 +19,28 @@
   (cf/room-by-name cf-settings room-name))
 
 (defn build-messages
-  [{:keys [report env app]}]
+  ""
+  [{:keys [app env report]}]
   (cond-> []
           env (conj (str "Environment: " env))
           app (conj (str "App: " app))
           report (into (map :message report))))
 
 (defn info
+  "Sends an info message to the Campfire room"
   [report]
-  (when (and (seq (:report report)) (not (env/env :service-campfire-off)))
+  (when (and (seq (:report report)) (not campfire-off?))
     (doseq [message (build-messages report)]
-      (cf/message (room (env/env :service-campfire-default-info-room)) message))))
+      (cf/message (room default-info-room) message))))
 
 (defn- ignore?
+  "Should we ignore the error?"
   [error-map]
   (>= (or (:status error-map) 0) 500))
 
 (defn error
-  "Sends error to campfire room"
+  "Sends an error message to the Campfire room"
   [error-map]
-  (when-not (or (env/env :service-campfire-off) (ignore? error-map))
+  (when-not (or campfire-off? (ignore? error-map))
     (doseq [error-message error-map]
-      (cf/message (room (env/env :service-campfire-default-info-room)) (str (first error-message) " " (second error-message))))))
+      (cf/message (room default-info-room) (str (first error-message) " " (second error-message))))))
--- test/shuppet/campfire_test.clj
+++ test/shuppet/campfire_test.clj
@@ -1,35 +1,58 @@
 (ns shuppet.campfire-test
   (:require [clj-campfire.core :as cf]
-            [environ.core :as env]
+            [environ.core :refer [env]]
             [midje.sweet :refer :all]
             [shuppet.campfire :refer :all]))
 
+(def campfire-before
+  (intern 'shuppet.campfire 'campfire-off?))
+
+(intern 'shuppet.campfire 'campfire-off? false)
+
+(fact "that we can get a room"
+      (room "the-room") => "room-id"
+      (provided
+       (cf/room-by-name {:api-token "campfire-api-token"
+                         :ssl true
+                         :sub-domain "campfire-sub-domain"} "the-room") => "room-id"))
+
 (fact "that info messages can be sent"
-      (info {:env "env"
-             :app "app"
-             :report [{:action ..action..
-                       :message "message1"}
-                      {:action ..action..
-                       :message "message2"}]}) => anything
-                       (provided
-                        (cf/message ..room.. #"message1") => nil
-                        (cf/message ..room.. #"message2") => nil
-                        (env/env anything) => nil
-                        (room anything) => ..room..
-                        (cf/message anything #"env") => nil
-                        (cf/message anything #"app") => nil))
+      (info {:env "env" :app "app" :report [{:message "message1"}
+                                            {:message "message2"}]})
+      => nil
+      (provided
+       (cf/message ..room.. #"message1") => nil
+       (cf/message ..room.. #"message2") => nil
+       (room "info-room") => ..room..
+       (cf/message ..room.. #"env") => nil
+       (cf/message ..room.. #"app") => nil))
 
 (fact "that error messages can be sent"
-      (error {:title "title"
-              :message "message"}) => anything
-              (provided
-               (env/env anything) => nil
-               (room anything) => ..room..
-               (cf/message ..room.. #"title") => nil
-               (cf/message ..room.. #"message") => nil))
+      (error {:message "message"})
+      => nil
+      (provided
+       (room "info-room") => ..room..
+       (cf/message ..room.. #"message") => nil))
 
 (fact "that 500s are never sent to Campfire"
-      (error {:status 500}) => anything
+      (error {:status 500})
+      => nil
       (provided
-       (env/env anything) => nil
        (cf/message anything anything) => nil :times 0))
+
+(intern 'shuppet.campfire 'campfire-off? true)
+
+(fact "that info messages can be sent"
+      (info {:env "env" :app "app" :report [{:message "message1"}
+                                            {:message "message2"}]})
+      => nil
+      (provided
+       (cf/message anything anything) => nil :times 0))
+
+(fact "that error messages are not sent when Campfire is off"
+      (error {:message "message"})
+      => nil
+      (provided
+       (cf/message anything anything) => nil :times 0))
+
+(intern 'shuppet.campfire 'campfire-off? campfire-before)
