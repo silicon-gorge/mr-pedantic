a7fcde6769b12cec005b80ab6cdd63a8ffe8e8a1
Neil Prosser <neil.prosser@gmail.com>
2015-11-02 10:46:07 +0000

    Metrics and configurability for backoff

--- src/pedantic/guard.clj
+++ src/pedantic/guard.clj
@@ -1,10 +1,26 @@
 (ns pedantic.guard
   (:require [clojure.tools.logging :refer [warn]]
-            [environ.core :refer [env]])
+            [environ.core :refer [env]]
+            [metrics.meters :refer [mark! meter]])
   (:import [com.amazonaws AmazonServiceException]))
+
+(def retried-error-meter
+  (meter ["info" "application" "aws.exceptions.retried"]))
+
+(def thrown-error-meter
+  (meter ["info" "application" "aws.exceptions.thrown"]))
 
 (def backoff-enabled?
   (Boolean/valueOf (env :backoff-enabled)))
+
+(def backoff-base-time
+  (Integer/valueOf (env :backoff-base-time-millis 100)))
+
+(def backoff-maximum
+  (Integer/valueOf (env :backoff-maximum-millis 10000)))
+
+(def backoff-rate
+  (Integer/valueOf (env :backoff-rate 2)))
 
 (defn- aws-request-limit-exceeded?
   [e]
@@ -23,19 +39,23 @@
 
 ; Code based on http://www.lispcast.com/exponential-backoff
 (defn exponential-backoff [time rate max matcher f]
-  (if (or (not backoff-enabled?)
-          (>= time max))
-    (f)
-    (try
+  (try
+    (if (or (not backoff-enabled?)
+            (>= time max))
       (f)
-      (catch Throwable t
-        (if (matcher t)
-          (do
-            (warn t "Caught retryable error which will be retried.")
-            (Thread/sleep time)
-            (exponential-backoff (* time rate) rate max matcher f))
-          (throw t))))))
+      (try
+        (f)
+        (catch Throwable t
+          (if (matcher t)
+            (do
+              (warn t "Caught retryable error which will be retried.")
+              (mark! retried-error-meter)
+              (Thread/sleep time)
+              (exponential-backoff (* time rate) rate max matcher f))
+            (throw t)))))
+    (finally
+      (mark! thrown-error-meter))))
 
 (defmacro guarded
   [& body]
-  `(exponential-backoff 100 2 10000 retryable-error? (fn [] ~@body)))
+  `(exponential-backoff backoff-base-time backoff-rate backoff-maximum retryable-error? (fn [] ~@body)))
